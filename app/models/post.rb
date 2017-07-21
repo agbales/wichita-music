7b94b6c2dfb8f911e94c8d4ce2f5a7a80a52e49a
Andrew Bales <agbales@gmail.com>
2017-07-20 18:09:52 -0700

    trying to use new models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :band_members
   has_one :album
+  has_one :band, through: :album
+  has_many :songs, through: :album
   mount_uploader :cover_image, ImageUploader
+  mount_uploader :song, AudioUploader
+  accepts_nested_attributes_for :album, :band, :band_members, :songs
 end
