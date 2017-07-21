fa573e4e4591213bf48f13c346b42a96c7547b46
Andrew Bales <agbales@gmail.com>
2017-07-21 12:22:56 -0700

    adding audio to song

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170721014808) do
+ActiveRecord::Schema.define(version: 20170721192156) do
 
   create_table "albums", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20170721014808) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "title"
+    t.string   "audio"
   end
 
   create_table "users", force: :cascade do |t|
